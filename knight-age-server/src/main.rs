cb075a002cd827c5bc1d7d735f8dc08411e467e4
admin <bombi19m6@gmail.com>
2025-12-30 22:17:28 +0700

    Refactor main function to separate networking logic into its own async function and improve error handling for connection acceptance

--- knight-age-server/src/main.rs
+++ knight-age-server/src/main.rs
@@ -7,24 +7,36 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind(("127.0.0.1", 19129)).await?;
+    starting_networking().await;
+    Ok(())
+}
+
+async fn starting_networking() {
+    let listener = TcpListener::bind(("127.0.0.1", 19129)).await.expect("Failed to bind");
 
     loop {
-        let (socket, addr) = listener.accept().await?;
+        match listener.accept().await {
+            Err(e) => {
+                eprintln!("Failed to accept connection: {}", e);
+                continue;
+            }
+            Ok((socket, addr)) => {
+                let (read_half, write_half) = socket.into_split();
 
-        let (read_half, write_half) = socket.into_split();
+                let (tx, rx): (mpsc::Sender<Packet>, mpsc::Receiver<Packet>) = mpsc::channel(100);
 
-        let (tx, rx): (mpsc::Sender<Packet>, mpsc::Receiver<Packet>) = mpsc::channel(100);
+                let(hehe, hihi ): (i16, i16) = (5, 10);
 
-        let(hehe, hihi ): (i16, i16) = (5, 10);
+                tokio::spawn(async move {
+                    // connection_reader_loop(conn_reader, tx).await;
+                });
 
-        tokio::spawn(async move {
-            // connection_reader_loop(conn_reader, tx).await;
-        });
+                tokio::spawn(async move {
+                    // let mut session = Session::new(rx, conn_writer);
+                    // session.run().await;
+                });
 
-        tokio::spawn(async move {
-            // let mut session = Session::new(rx, conn_writer);
-            // session.run().await;
-        });
+            }
+        }
     }
 }
