bf528ee93ff4ac020cfa8d0a9e41a6229afc0e41
admin <bombi19m6@gmail.com>
2026-01-08 01:19:38 +0700

    Remove network module and simplify main function to a placeholder

--- knight-age-server/src/main.rs
+++ knight-age-server/src/main.rs
@@ -1,15 +1,1 @@
-mod network;
-use std::error::Error;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    println!("===========================================");
-    println!("     Knight Age Server - Starting...       ");
-    println!("===========================================");
-
-    network::start_net_work().await?;
-    Ok(())
-}
-
-
-
+fn main() {}
